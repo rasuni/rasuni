f82f5f7cb9260f80fe403f4ecd1df2813ba31664
rasuni <rasuni.git@gmail.com>
2015-02-11 01:33:34 +0100

    rasuni.test.Classes

---
+++ test/src/main/java/rasuni/test/Classes.java
@@ -0,0 +1,52 @@
+package rasuni.test;
+
+import java.lang.reflect.Constructor;
+import java.lang.reflect.InvocationTargetException;
+
+/**
+ * Test utilities for classes
+ *
+ */
+public class Classes
+{
+	/**
+	 * Construct an instance of the specified class
+	 * @param cls the class
+	 */
+	public static void construct(Class<?> cls)
+	{
+		try
+		{
+			Constructor<?> constructor = cls.getDeclaredConstructor();
+			constructor.setAccessible(true);
+			try
+			{
+				constructor.newInstance();
+			}
+			catch (InstantiationException e)
+			{
+				throw new RuntimeException(e);
+			}
+			catch (IllegalAccessException e)
+			{
+				throw new RuntimeException(e);
+			}
+			catch (IllegalArgumentException e)
+			{
+				throw new RuntimeException(e);
+			}
+			catch (InvocationTargetException e)
+			{
+				throw new RuntimeException(e);
+			}
+		}
+		catch (NoSuchMethodException e)
+		{
+			throw new RuntimeException(e);
+		}
+		catch (SecurityException e)
+		{
+			throw new RuntimeException(e);
+		}
+	}
+}
