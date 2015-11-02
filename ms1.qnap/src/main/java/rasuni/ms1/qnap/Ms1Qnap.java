bb6b864e6daa50e39f06bcd104efa37ab3eb16a1
rasuni <rasuni.git@gmail.com>
2015-11-02 19:42:48 +0100

    makeVertexLabel

--- ms1.qnap/src/main/java/rasuni/ms1/qnap/Ms1Qnap.java
+++ ms1.qnap/src/main/java/rasuni/ms1/qnap/Ms1Qnap.java
@@ -1,9 +1,7 @@
 package rasuni.ms1.qnap;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import org.apache.log4j.helpers.LogLog;
-import rasuni.titan.TitanCollector;
+import fj.data.Array;
+import rasuni.titan.MusicCollector;
 
 /**
  * Qnap collection
@@ -18,10 +16,6 @@ public class Ms1Qnap // NO_UCD (unused code)
 	 */
 	public static void main(String[] args)
 	{
-		LinkedList<Iterable<String>> roots = new LinkedList<>();
-		roots.add(Arrays.asList("\\\\MusikServer", "Musik"));
-		roots.add(Arrays.asList("\\\\qnap", "music"));
-		roots.add(Arrays.asList("\\\\qnap", "Qmultimedia"));
-		TitanCollector.run(System.getSecurityManager(), System.getProperties(), LogLog.g_debugEnabled, LogLog.g_quietMode, System.out, "ms1qnap", roots, true, Arrays.asList("\\\\qnap", "Qmultimedia"));
+		MusicCollector.run(Array.array("\\\\MusikServer\\Musik", "\\\\qnap\\music", "\\\\qnap\\Qmultimedia"), "ms1qnap", System.out, "\\\\MusikServer\\Musik", true);
 	}
 }
--- rasuni-titan/src/main/java/rasuni/titan/TitanCollector.java
+++ rasuni-titan/src/main/java/rasuni/titan/TitanCollector.java
@@ -271,7 +271,7 @@ public final class TitanCollector
 		return ifNull(sequence, null, (IProvider<ISequence<String>>) () ->
 		{
 			return new ISequence<String>()
-			{
+					{
 				@Override
 				public String getHead()
 				{
@@ -283,7 +283,7 @@ public final class TitanCollector
 				{
 					return map(sequence.getTail(), toString);
 				}
-			};
+					};
 		});
 	}
 
@@ -324,7 +324,7 @@ public final class TitanCollector
 	private static <T> ISequence<T> sequence(T[] array, int pos)
 	{
 		return array.length == pos ? null : new ISequence<T>()
-		{
+				{
 			@Override
 			public T getHead()
 			{
@@ -336,7 +336,7 @@ public final class TitanCollector
 			{
 				return sequence(array, pos + 1);
 			}
-		};
+				};
 	}
 
 	/**
@@ -486,9 +486,9 @@ public final class TitanCollector
 	{
 		return includeEntity(entity, Entity::getId, resource, tg, out, Object::toString, mbid ->
 		{ // empty
-				}, vertex ->
-				{ // empty
-				});
+		}, vertex ->
+		{ // empty
+		});
 	}
 
 	private static void printSpace(PrintStream out, String s)
@@ -899,7 +899,7 @@ public final class TitanCollector
 					{
 						return includeEntity11(target1 -> target1._id, target, Resource.URL, tg1, System.out, target1 -> target1._target, v ->
 						{ // empty
-								}, v -> false, v -> true);
+						}, v -> false, v -> true);
 					}, tg, relation -> relation._target))
 					{
 						return false;
@@ -1060,7 +1060,7 @@ public final class TitanCollector
 			{
 				includeEntity(idl, entity, resource, tg, System.out, name, mbid ->
 				{ // empty
-						}, v -> null, v -> null);
+				}, v -> null, v -> null);
 				removeCurrent(tg, "merged", foreignId, entityId);
 				return false;
 			}
@@ -1091,27 +1091,18 @@ public final class TitanCollector
 		});
 	}
 
-	public static boolean browseArtists(final WebService<MetaData> musicBrainz, final Parameter parameter, final TitanGraph tg)
-	{
-		return browse(ResourceDefinition.ARTIST, musicBrainz, Resource.ARTIST, parameter, v ->
-		{ // empty
-				}, tg, mbid ->
-				{ // empty
-				});
-	}
-
 	public static boolean browseReleaseGroups(final WebService<MetaData> musicBrainz, final Parameter param, IConsumer<Vertex> checkVertex, final TitanGraph tg)
 	{
 		return browse(ResourceDefinition.RELEASE_GROUP, musicBrainz, Resource.RELEASE_GROUP, param, checkVertex, tg, mbid ->
 		{ // empty
-				});
+		});
 	}
 
 	public static boolean browseRecordings(final WebService<MetaData> musicBrainz, final Parameter param, final TitanGraph tg, IConsumer<String> inspect)
 	{
 		return browse(ResourceDefinition.RECORDING, musicBrainz, Resource.RECORDING, param, v ->
 		{ // empty
-				}, tg, inspect);
+		}, tg, inspect);
 	}
 
 	public static boolean browseReleases(final WebService<MetaData> musicBrainz, final Parameter parameter, IConsumer<Vertex> checkVertex, final TitanGraph tg, IConsumer<String> inspect)
