1bc9d4bdd897333dac3b0bdf87e4ad653b049f38
rasuni <rasuni.git@gmail.com>
2015-11-17 20:23:32 +0100

    makeIntPropertyKey

--- ms1music/src/main/java/ms1/music/Ms1Music.java
+++ ms1music/src/main/java/ms1/music/Ms1Music.java
@@ -1,9 +1,7 @@
 package ms1.music;
 
-import java.util.Arrays;
-import java.util.LinkedList;
-import org.apache.log4j.helpers.LogLog;
-import rasuni.titan.TitanCollector;
+import fj.data.Array;
+import rasuni.titan.MusicCollector;
 
 /**
  * Qnap collection
@@ -18,9 +16,6 @@ public class Ms1Music // NO_UCD (unused code)
 	 */
 	public static void main(String[] args)
 	{
-		LinkedList<Iterable<String>> roots = new LinkedList<>();
-		roots.add(Arrays.asList("\\\\MusikServer", "Musik"));
-		roots.add(Arrays.asList("\\\\qnap", "music"));
-		TitanCollector.run(System.getSecurityManager(), System.getProperties(), LogLog.g_debugEnabled, LogLog.g_quietMode, System.out, "ms1music", roots, true, Arrays.asList("\\\\qnap", "music"));
+		MusicCollector.run(Array.array("\\\\MusikServer\\Musik", "\\\\qnap\\music"), "ms1music", System.out, "\\\\MusikServer\\Musik", true);
 	}
 }
