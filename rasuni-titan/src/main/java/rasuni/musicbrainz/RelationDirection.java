713a7c96c7eb24092e7f5aab8aa7d1494fdf848d
rasuni <rasuni.git@gmail.com>
2016-01-18 22:26:31 +0100

    Query Labels

--- rasuni-titan/src/main/java/rasuni/musicbrainz/RelationDirection.java
+++ rasuni-titan/src/main/java/rasuni/musicbrainz/RelationDirection.java
@@ -4,9 +4,18 @@ import javax.xml.bind.annotation.XmlEnumValue;
 
 public enum RelationDirection
 {
+	/**
+	 *
+	 */
+	@XmlEnumValue("both")
+	BOTH, // NO_UCD (unused code)
+	/**
+	 *
+	 */
+	@XmlEnumValue("forward")
 	FORWARD,
 	/**
-	 * backward
+	 *
 	 */
 	@XmlEnumValue("backward")
 	BACKWARD,
