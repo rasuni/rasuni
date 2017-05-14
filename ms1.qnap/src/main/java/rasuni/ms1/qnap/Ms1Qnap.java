f2fc4902943a9f5e0536257b5bfe213527a0aa1f
ralph.sigrist <ralph.sigrist@192.168.1.20>
2017-05-14 09:13:52 +0200

    Arrays.ArrayList.size

--- ms1.qnap/src/main/java/rasuni/ms1/qnap/Ms1Qnap.java
+++ ms1.qnap/src/main/java/rasuni/ms1/qnap/Ms1Qnap.java
@@ -1,9 +1,13 @@
 package rasuni.ms1.qnap;
 
 import java.util.AbstractList;
+import java.util.NoSuchElementException;
+import org.pcollections.ConsPStack;
+import rasuni.Stack;
 import rasuni.java.lang.Classes;
 import rasuni.java.lang.Objects;
 import rasuni.java.util.AbstractLists;
+import rasuni.java.util.Arrays;
 import rasuni.org.pcollections.ConsPStacks;
 import rasuni.sun.misc.Unsafe;
 import rasuni.titan.MusicCollector;
@@ -25,6 +29,30 @@ public class Ms1Qnap // NO_UCD (unused code)
 		final AbstractList<String> arrayList = (AbstractList<String>) Unsafe.allocateInstance(Classes.forName("java.util.Arrays$ArrayList"));
 		Objects.set(arrayList, "modCount", 0);
 		Objects.set(arrayList, "a", new String[] { "/Volumes/Musik", "/Volumes/music", "/Volumes/Qmultimedia" });
-		MusicCollector.run(ConsPStacks.from(arrayList, AbstractLists::iterator, AbstractLists.Itr::hasNext, i -> AbstractLists.Itr.next(i)), "ms1qnap", "/Volumes/Musik", true);
+		int cursor = 0;
+		final int expectedModCount = 0;
+		Stack<String> stack = null;
+		while (cursor != Arrays.ArrayList.size(arrayList))
+		{
+			AbstractLists.checkForComodification(arrayList, expectedModCount);
+			try
+			{
+				stack = new Stack<>(arrayList.get(cursor), stack);
+			}
+			catch (final IndexOutOfBoundsException e)
+			{
+				AbstractLists.checkForComodification(arrayList, expectedModCount);
+				throw new NoSuchElementException();
+			}
+			cursor++;
+		}
+		@SuppressWarnings("unchecked")
+		ConsPStack<String> result = (ConsPStack<String>) ConsPStacks.EMPTY;
+		while (stack != null)
+		{
+			result = ConsPStacks.plus(result, stack._element);
+			stack = stack._next;
+		}
+		MusicCollector.run(result, "ms1qnap", "/Volumes/Musik", true);
 	}
 }
--- ms1music/src/main/java/ms1/music/Ms1Music.java
+++ ms1music/src/main/java/ms1/music/Ms1Music.java
@@ -1,8 +1,6 @@
 package ms1.music;
 
 import java.util.AbstractList;
-import java.util.Iterator;
-import java.util.function.Function;
 import rasuni.java.lang.Classes;
 import rasuni.java.lang.Objects;
 import rasuni.java.util.AbstractLists;
@@ -27,6 +25,6 @@ public class Ms1Music // NO_UCD (unused code)
 		final AbstractList<String> arrayList = (AbstractList<String>) Unsafe.allocateInstance(Classes.forName("java.util.Arrays$ArrayList"));
 		Objects.set(arrayList, "modCount", 0);
 		Objects.set(arrayList, "a", new String[] { "/Volumes/Musik", "/Volumes/music" });
-		MusicCollector.run(ConsPStacks.from(arrayList, (Function<AbstractList<String>, Iterator<String>>) AbstractLists::iterator, AbstractLists.Itr::hasNext, AbstractLists.Itr::next), "ms1music", "/Volumes/Musik", true);
+		MusicCollector.run(ConsPStacks.from(arrayList, list -> AbstractLists.iterator(list), AbstractLists.Itr::hasNext, AbstractLists.Itr::next), "ms1music", "/Volumes/Musik", true);
 	}
 }
